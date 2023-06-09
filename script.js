257a7716518eb439182594685cc74f52b1ca16a2
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-06-09 20:19:59 +0800

    Remove Empty Spaces In html, Create Canvas Dimensions Function, And Remove Unnecessary Elements

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const contain = document.querySelector('.square-container');
 
-//Starting dimensions
-for (let i=0; i<16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    contain.appendChild(row);
-    for (let i=0; i<16; i++) {
+canvasDimensions(16);
+
+function canvasDimensions(squares) {
+    for (let i=0; i<squares**2; i++) {
         const temp = document.createElement('div');
         temp.classList.add('square');
-        let whMeasure = 400 / 16;
+        let whMeasure = 400 / squares;
         temp.style.width = `${whMeasure}px`
         temp.style.height = `${whMeasure}px`
-        row.appendChild(temp);
+        contain.appendChild(temp);
     }
 }
