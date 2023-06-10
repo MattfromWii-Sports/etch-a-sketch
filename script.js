6c9d000a0787472fb9f16ff1695d9e3cae2af256
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-06-10 11:54:47 +0800

    Add html Buttons For Sidebar, And Adjust Outline Of Squares To Be Lighter

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function canvasDimensions(squares) {
     for (let i=0; i<squares**2; i++) {
         const temp = document.createElement('div');
         temp.classList.add('square');
-        let whMeasure = 400 / squares;
-        temp.style.width = `${whMeasure}px`
-        temp.style.height = `${whMeasure}px`
+        let whMeasure = 100 / squares;
+        temp.style.width = `${whMeasure}%`
+        temp.style.height = `${whMeasure}%`
         contain.appendChild(temp);
     }
     allSquares = document.querySelectorAll('.square');
