9c2b4bcbd418e88607611682246b41c5e4b4e9a2
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-06-12 16:00:28 +0800

    Change Id Of Buttons, And Add Highlight For Reset Button

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let allSquares;
+let colorMode = 'black';
 const contain = document.querySelector('.square-container');
-const resetBtn = document.getElementById('reset');
+const resetBtn = document.getElementById('reset-b');
+const rainbowBtn = document.getElementById('rainbow-color');
+const blackBtn = document.getElementById('black-color');
 
 canvasDimensions(16);
 
-allSquares.forEach(sqr => sqr.addEventListener('mouseover', colorChange));
-resetBtn.addEventListener('click', resetCanvas);
+allSquares.forEach(sqr => sqr.addEventListener('mouseover', function() {
+    this.style.backgroundColor = `${colorMode}`;
+}));
+resetBtn.addEventListener('mousedown', resetCanvas);
 
 function canvasDimensions(squares) {
     for (let i=0; i<squares**2; i++) {
@@ -19,12 +24,12 @@ function canvasDimensions(squares) {
     allSquares = document.querySelectorAll('.square');
 }
 
-function colorChange() {
-    this.style.backgroundColor = 'black';
-}
-
 function resetCanvas() {
     for (let i=0; i<allSquares.length; i++) {
         allSquares[i].style.backgroundColor = 'white';
     }
+    resetBtn.style.backgroundColor = 'yellow';
+    resetBtn.addEventListener('mouseup', function() {
+        resetBtn.style.backgroundColor = 'white';
+    });
 }
